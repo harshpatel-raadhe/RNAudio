c6478ee057642c8043e78d78b5416dbbdb9e8dd9
Harsh <hnpatel9026@gmail.com>
2022-03-24 18:29:38 +0530

    Initail Commit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { AppRegistry } from 'react-native';
-import App from './build/App/App';
+import App from './src/App/App';
 import { name as appName } from './app.json';
 import TrackPlayer from 'react-native-track-player';
 
 AppRegistry.registerComponent(appName, () => App);
 
-TrackPlayer.registerPlaybackService(() => require('./build/utilsAndServices/TrackPlayerService'));
+TrackPlayer.registerPlaybackService(() => require('./src/utilsAndServices/TrackPlayerService'));
